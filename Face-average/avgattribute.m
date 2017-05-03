8a82cd6baaa78b8174b8dc1da7c60a58f2b45ee3
dhingratul <atul.dhingra@rutgers.edu>
2017-05-03 10:49:06 -0400

    Face-Avg project

--- Face-average/avgattribute.m
+++ Face-average/avgattribute.m
@@ -1,6 +1,12 @@
+%% Computes the average face from PubFig dataset based on attributes
+%---------------------------------------------------------------------- %
+% Author: Atul Dhingra
+% dhingra[dot]atul92[at]gmail.com
+% ---------------------------------------------------------------------- %
 % function [avg]=avgattribute()
 clear all; close all; clc;
 avg=zeros(100,100,3);count=0; S_old=''; a=3;%Male face
+addpath /home/labuser/Dropbox/Research/FaceRecognition/Dataset/Pubfig;
 load pubfigattributes_c.mat;
 pubfigattributes=pubfigattributes(2:end,:);
 srcFiles = dir('/home/dhingratul/Dropbox/Research/FaceVerification/Dataset/PubFig/*.jpg');
@@ -8,23 +14,23 @@ srcFiles = dir('/home/dhingratul/Dropbox/Research/FaceVerification/Dataset/PubFi
 for i = 1 : length(srcFiles)
     filename = strcat('/home/dhingratul/Dropbox/Research/FaceVerification/Dataset/PubFig/',srcFiles(i).name);
     [C]=strsplit(srcFiles(i).name,'_');
-    S=strcat(C{1,1},{' '},C{1,2}); 
+    S=strcat(C{1,1},{' '},C{1,2});
     D=strsplit(C{1,3},'.');
-        img =imread(filename);
-        img=imresize(img,[100,100]);
-        cmp=strcmp(pubfigattributes(:,1),S{1,1});
-        idx=find(cmp==1);
-        index=find(cell2mat(pubfigattributes(idx(1):idx(end),2))==str2num(D{1,1}));
-                if(pubfigattributes{index,a}>0)
-                count=count+1;
-                avg=avg+pubfigattributes{index,a}*double(img);
-                end        
+    img =imread(filename);
+    img=imresize(img,[100,100]);
+    cmp=strcmp(pubfigattributes(:,1),S{1,1});
+    idx=find(cmp==1);
+    index=find(cell2mat(pubfigattributes(idx(1):idx(end),2))==str2num(D{1,1}));
+    if(pubfigattributes{index,a}>0)
+        count=count+1;
+        avg=avg+pubfigattributes{index,a}*double(img);
+    end
 end
-       avg=avg/count;
-       avg=uint8(avg);
-%        imshow(uint8(avg));
-      save(['avg_' num2str(a) '.mat'],'avg');
-      avg=zeros(100,100,3);count=0;
+avg=avg/count;
+avg=uint8(avg);
+imshow(uint8(avg));
+%       save(['avg_' num2str(a) '.mat'],'avg');
+%       avg=zeros(100,100,3);count=0;
 % end
 % end %Function End
 
--- Face-average/lincombface.m
+++ Face-average/lincombface.m
@@ -1,27 +1,32 @@
+%% Script to output faces as a linear combination of others
+% ---------------------------------------------------------------------- %
+% Author: Atul Dhingra
+% dhingra[dot]atul92[at]gmail.com
+% ---------------------------------------------------------------------- %
 function [img]=lincombface(vector)
-    clear all; close all; clc;
-    avg=zeros(100,100,3);count=0; 
-    load pubfigattributes_c.mat;
-    pubfigattributes=pubfigattributes(2:end,:);
-    srcFiles = dir('/home/dhingratul/Dropbox/Research/FaceVerification/Dataset/PubFig/*.jpg');
-    % for a=16:size(pubfigattributes,2)% the folder in which ur images exists
-    for i = 1 : length(srcFiles)
+clear all; close all; clc;
+avg=zeros(100,100,3);count=0;
+load pubfigattributes_c.mat;
+pubfigattributes=pubfigattributes(2:end,:);
+srcFiles = dir('/home/labuser/Documents/Dataset/pubfig83/*.jpg');
+% for a=16:size(pubfigattributes,2)% the folder in which ur images exists
+for i = 1 : length(srcFiles)
     filename = strcat('/home/dhingratul/Dropbox/Research/FaceVerification/Dataset/PubFig/',srcFiles(i).name);
     [C]=strsplit(srcFiles(i).name,'_');
-    S=strcat(C{1,1},{' '},C{1,2}); 
+    S=strcat(C{1,1},{' '},C{1,2});
     D=strsplit(C{1,3},'.');
-        img =imread(filename);
-        img=imresize(img,[100,100]);
-        cmp=strcmp(pubfigattributes(:,1),S{1,1});
-        idx=find(cmp==1);
-        index=find(cell2mat(pubfigattributes(idx(1):idx(end),2))==str2num(D{1,1}));
-                if(pubfigattributes{index,a}>0)
-                count=count+1;
-                avg=avg+pubfigattributes{index,a}*double(img);
-                end        
+    img =imread(filename);
+    img=imresize(img,[100,100]);
+    cmp=strcmp(pubfigattributes(:,1),S{1,1});
+    idx=find(cmp==1);
+    index=find(cell2mat(pubfigattributes(idx(1):idx(end),2))==str2num(D{1,1}));
+    if(pubfigattributes{index,a}>0)
+        count=count+1;
+        avg=avg+pubfigattributes{index,a}*double(img);
+    end
 end
-       avg=avg/count;
-       avg=uint8(avg);
-       save(['avg_' num2str(a) '.mat'],'avg');
-       avg=zeros(100,100,3);count=0;
+avg=avg/count;
+avg=uint8(avg);
+%save(['avg_' num2str(a) '.mat'],'avg');
+avg=zeros(100,100,3);count=0;
 end
