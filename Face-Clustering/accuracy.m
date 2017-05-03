fd061c663680d5d50bc5f234a1ca37887b6c8303
dhingratul <atul.dhingra@rutgers.edu>
2017-05-03 10:53:20 -0400

    Pairwise Precision, Recall

--- Face-Clustering/accuracy.m
+++ Face-Clustering/accuracy.m
@@ -1,3 +1,6 @@
+%% Function to compute pairwise precision and recall
+% Input = ground truth classes, index from clustering
+% Output = pairwise precision recall
 function[precall,pprecision]=accuracy(gnd,idx)
 %% Accuracy calculation
 numClusters=max(idx); count=0; numerator=0; denominator=0; scp=0;
@@ -10,19 +13,19 @@ for k=1:numClusters
     for i=1:size(temp,1)
         predictedLabel(i,1)=gnd(temp(i,1));
     end
- 
+    
     for i=1:size(predictedLabel,1)
         
         num=nnz(find(predictedLabel==i));
         if(num>1)
             numerator=numerator+nchoosek(num,2);
         end
-    end    
+    end
 end
 for i=1:size(gnd,1)
     num=nnz(find(idx==i));
     if(num>1)
-    denominator=denominator+nchoosek(num,2);
+        denominator=denominator+nchoosek(num,2);
     end
 end
 pprecision=numerator/scp
